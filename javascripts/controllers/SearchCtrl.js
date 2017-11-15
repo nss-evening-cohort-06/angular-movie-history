5e9a2b04917795a682fb46380232655afdfc94c4
Zoe Ames <zoeames@gmail.com>
2017-11-14 19:13:00 -0600

    search working

--- javascripts/controllers/SearchCtrl.js
+++ javascripts/controllers/SearchCtrl.js
@@ -1,5 +1,22 @@
 'use strict';
 
-app.controller("SearchCtrl", function($scope){
-  $scope.controller = "SearchCtrl";
+app.controller("SearchCtrl", function($scope, tmdbService){
+  
+  $scope.enterPush = (event) =>{
+    if(event.keyCode === 13){
+      tmdbService.searchMovies(event.target.value).then((results) =>{
+        console.log("movies?", results.data.results);
+      }).catch((err) =>{
+        console.log("error in searchMovies", err);
+      });
+    }
+  };
+  
+  
+  
+
+
+
+
+
 });
