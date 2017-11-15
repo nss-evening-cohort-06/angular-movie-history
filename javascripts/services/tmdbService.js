48c1b6014c4ecd546df1e55d736f4d39dc8969cd
Zoe Ames <zoeames@gmail.com>
2017-11-14 20:33:54 -0600

    images showing

--- javascripts/services/tmdbService.js
+++ javascripts/services/tmdbService.js
@@ -5,7 +5,9 @@ app.service("tmdbService", function($http, TMDB_CONFIG){
     return $http.get(`https://api.themoviedb.org/3/search/movie?api_key=${TMDB_CONFIG}&language=en-US&page=1&include_adult=false&query=${query}`);
   };
 
+  const tmdbConfiguration = () => {
+    return $http.get(`https://api.themoviedb.org/3/configuration?api_key=${TMDB_CONFIG}`);
+  };
 
-
-  return {searchMovies};
+  return {searchMovies, tmdbConfiguration};
 });
