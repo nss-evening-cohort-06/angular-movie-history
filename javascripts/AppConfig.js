2d551c216f48b8da3c0665162a860e79428b6f9d
Zoe Ames <zoeames@gmail.com>
2017-11-15 18:57:40 -0600

    done

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -1,6 +1,14 @@
 'use strict';
+let isAuth = (AuthService) => new Promise ((resolve, reject) => {
+  if(AuthService.isAuthenticated()){
+    resolve();
+  } else {
+    reject();
+  }
+});
 
-app.run(function($rootScope, FIREBASE_CONFIG, tmdbService){
+
+app.run(function($location, $rootScope, FIREBASE_CONFIG, AuthService, tmdbService){
   firebase.initializeApp(FIREBASE_CONFIG);
 
   tmdbService.tmdbConfiguration().then((result)=>{
@@ -8,6 +16,38 @@ app.run(function($rootScope, FIREBASE_CONFIG, tmdbService){
   }).catch((err) =>{
     console.log("error in tmdbConfiguration", err);
   });
+
+//watch method that fires on change of a route.  3 inputs. 
+  //event is a change event
+  //currRoute is information about your current route
+  //prevRoute is information about the route you came from
+  $rootScope.$on('$routeChangeStart', function(event, currRoute, prevRoute) {
+    // checks to see if there is a current user
+    var logged = AuthService.isAuthenticated();
+
+    var appTo;
+
+    // to keep error from being thrown on page refresh
+    if (currRoute.originalPath) {
+      // check if the user is going to the auth page = currRoute.originalPath
+      // if user is on auth page then appTo is true
+      // if it finds something other than /auth it return a -1 and -1!==-1 so resolves to false
+      
+      // currRoute.originalPath = '/search'   -1 !== -1   appTo = false
+      // currRoute.originalPath = '/auth'   0 !== -1   appTo =true
+      appTo = currRoute.originalPath.indexOf('/auth') !== -1;
+    }
+
+    //if not on /auth page AND not logged in redirect to /auth
+    if (!appTo && !logged) {
+      event.preventDefault();
+      $location.path('/auth');
+    }
+  });
+
+
+
+  
 });
 
 app.config(function($routeProvider){
@@ -18,15 +58,18 @@ app.config(function($routeProvider){
     })
     .when("/search", {
       templateUrl: 'partials/search.html',
-      controller: 'SearchCtrl'
+      controller: 'SearchCtrl',
+      resolve: {isAuth}
     })
     .when("/wishlist", {
       templateUrl: 'partials/wishlist.html',
-      controller: 'WishlistCtrl'
+      controller: 'WishlistCtrl',
+      resolve: {isAuth}
     })
     .when("/rated", {
       templateUrl: 'partials/rated.html',
-      controller: 'RatedCtrl'
+      controller: 'RatedCtrl',
+      resolve: {isAuth}
     })
     .otherwise('/auth');
 });
