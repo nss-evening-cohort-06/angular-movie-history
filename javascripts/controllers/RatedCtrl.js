e7b34f76b2169bb611a9f02e34ba579d43c97520
Zoe Ames <zoeames@gmail.com>
2017-11-15 19:40:45 -0600

    added rated

--- javascripts/controllers/RatedCtrl.js
+++ javascripts/controllers/RatedCtrl.js
@@ -1,5 +1,11 @@
 'use strict';
 
-app.controller("RatedCtrl", function($scope){
-  $scope.controller = "RatedCtrl";
+app.controller("RatedCtrl", function($rootScope, $scope, MovieService){
+  $scope.movies = [];
+
+  MovieService.getRatedMovies($rootScope.uid).then((results) =>{
+    $scope.movies = results;
+  }).catch((err) =>{
+    console.log("error in getRatedMovies", err);
+  });
 });
