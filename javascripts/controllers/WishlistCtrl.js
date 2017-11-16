eb3c6f04a431b8839c72aafb8e38f4d87e2a3ef6
Zoe Ames <zoeames@gmail.com>
2017-11-15 21:30:08 -0600

    delete works

--- javascripts/controllers/WishlistCtrl.js
+++ javascripts/controllers/WishlistCtrl.js
@@ -2,9 +2,21 @@
 
 app.controller("WishlistCtrl", function($rootScope, $scope, MovieService){
   
-  MovieService.getWishlistMovies($rootScope.uid).then((results) =>{
-    $scope.movies = results;
-  }).catch((err) =>{
-    console.log("error in getRatedMovies", err);
-  });
+  const getMovies = () => {
+    MovieService.getWishlistMovies($rootScope.uid).then((results) =>{
+      $scope.movies = results;
+    }).catch((err) =>{
+      console.log("error in getRatedMovies", err);
+    });
+  };
+  
+  getMovies();
+
+  $scope.deleteMovie = (movieId) => {
+    MovieService.deleteMovie(movieId).then((result) =>{
+      getMovies();
+    }).catch((err) =>{
+      console.log("error in deleteMovie", err);
+    });
+  };
 });
