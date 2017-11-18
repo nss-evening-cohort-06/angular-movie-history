d4c0e486141ee34534849cf71881789921d0e412
Zoe Ames <zoeames@gmail.com>
2017-11-18 09:48:27 -0600

    i watched it button works

--- javascripts/controllers/WishlistCtrl.js
+++ javascripts/controllers/WishlistCtrl.js
@@ -20,12 +20,11 @@ app.controller("WishlistCtrl", function($rootScope, $scope, MovieService){
     });
   };
 
-
   $scope.switchWatched = (movie) => {
     movie.isWatched = true;
-    console.log("movie", movie);
-    MovieService.updateMovie(movie, movie.id).then((result) =>{
-      console.log("result", result);
+    let updatedMovie = MovieService.createMovieObject(movie);
+    MovieService.updateMovie(updatedMovie, movie.id).then((result) =>{
+      getMovies();
     }).catch((err) =>{
       console.log("error in updateMovie", err);
     });
