d1ff8c5fa48ec0664472d13b86a4daaaea2eaa96
Zoe Ames <zoeames@gmail.com>
2017-11-18 11:27:29 -0600

    movie detail done

--- javascripts/controllers/MovieDetailCtrl.js
+++ javascripts/controllers/MovieDetailCtrl.js
@@ -1,5 +1,37 @@
 'use strict';
 
-app.controller("MovieDetailCtrl", function($scope){
-  $scope.hi = "hello";
+app.controller("MovieDetailCtrl", function($routeParams, $scope, MovieService){
+  $scope.movie = {};
+
+  const getMovie = () => {
+    MovieService.getSingleMovie($routeParams.id).then((results) =>{
+      $scope.movie = results.data;
+    }).catch((err) =>{
+      console.log("err in getSingleMovie", err);
+    });
+  };
+
+  getMovie();
+
+  $scope.switchWatched = (movie) => {
+    movie.isWatched = true;
+    let updatedMovie = MovieService.createMovieObject(movie);
+    MovieService.updateMovie(updatedMovie, $routeParams.id).then((result) =>{
+      getMovie();
+    }).catch((err) =>{
+      console.log("error in updateMovie", err);
+    });
+  };
+
+  $scope.starChange = (event, movie) => {
+    if(event.rating){
+      movie.rating = event.rating;
+      let updatedMovie = MovieService.createMovieObject(movie);
+      MovieService.updateMovie(updatedMovie, $routeParams.id).then(() =>{
+        getMovie();
+      }).catch((err) =>{
+        console.log("error in updateMovie", err);
+      });
+    }
+  };
 });
